1b8965c937084f9eb9c0f103b0c3c79164843c78
AndreyShvachko <an.shvachko@gmail.com>
2025-04-05 14:01:22 +0300

    firedman test added

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -36,6 +36,20 @@ def vigenere_decrypt(cipher, key):
             result += char
         return result
     
+def friedman_test(ciphertext):
+    text = ''.join(filter(str.isalpha, ciphertext.upper()))
+    N = len(text)
+    if N <= 1:
+        return 0, 0
+    
+    freq = Counter(text)
+    numerator = sum(f * (f-1) for f in freq.values())
+    denominator = N * (N - 1)
+    IC = numerator / denominator if denominator != 0 else 0
+
+    K = (0.0265 * N) / ((IC * (N - 1)) - 0.0385 * N + 0.065 * N)
+    return round(K, 2), round(IC, 4)
+    
 #Приклад
 plaintext = "CRYPTOGRAPHY IS FUN"
 key = "KEY"
@@ -43,9 +57,12 @@ key = "KEY"
 encrypted_vig = vigenere_encrypt(plaintext, key)
 frequencies_vig = frequency_analysis(encrypted_vig)
 decrypted_vig = vigenere_decrypt(encrypted_vig, key)
+estimated_key_length, ic_value = friedman_test(encrypted_vig)
 
 print("\n Vigenere Cipher")
 print("Original: ", plaintext)
 print("Encrypted: ", encrypted_vig)
 print("Frequencies: ", dict(frequencies_vig))
 print("Decrypted: ", decrypted_vig)
+print("IC:", ic_value)
+print("Estimated key length (Firedman):", estimated_key_length)
